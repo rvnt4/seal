da888843f71d3156a9e9bb30e0b1c6cbf3ef8bff
rev <rev@andromeda>
2026-09-22 12:42:16 -0300

    feat: handle failure count

--- src/test/main.cpp
+++ src/test/main.cpp
@@ -2,6 +2,15 @@
 
 int main()
 {
-    TestRunner::get().runTests();
+    TestRunner& runner = TestRunner::get();
+    runner.runTests();
+
+    if (runner.failureCount() > 0)
+    {
+        std::cout << "\x1b[97;41m" << runner.failureCount() << " test(s) failed" << "\x1b[0m" << std::endl;
+        return 1;
+    }
+
+    std::cout << "\x1b[32;40m" << "all tests passed" << "\x1b[0m" << std::endl;
     return 0;
 }
